d69fd7a4cffccf3c452c9a9dd97fcaf35b2ee60a
Noah Kernis <nkernis@gmail.com>
2020-02-06 16:15:40 -0500

    adding sonus lib

--- index.js
+++ index.js
@@ -1,60 +1,114 @@
-const RevAiApiClient = require('revai-node-sdk').RevAiApiClient;
 // const Gpio = require('pigpio').Gpio;
+const Sonus = require('sonus')
 
-const accessToken = process.env.REVAI_TOKEN;
-const client = new RevAiApiClient(accessToken);
+// const accessToken = process.env.REVAI_TOKEN;
 
-// 1.Submit file
-// you can submit a local file
-// var job = await client.submitJobLocalFile("./tmp/pills_00.mp4");
+const language = "en-US"
+const recordProgram = "arecord"
+const device = "plughw:1,0"
+const hotwords = [
+	{
+		file: './resources/Begging.pmdl',
+		hotword: 'begging'
+	},
+	{
+		file: './resources/Die.pmdl',
+		hotword: 'die'
+	},
+	{
+		file: './resources/Give.pmdl',
+		hotword: 'give'
+	},
+	{
+		file: './resources/Have.pmdl',
+		hotword: 'have'
+	},
+	{
+		file: './resources/Help.pmdl',
+		hotword: 'help'
+	},
+	{
+		file: './resources/Let.pmdl',
+		hotword: 'let'
+	},
+	{
+		file: './resources/Life.pmdl',
+		hotword: 'life'
+	},
+	{
+		file: './resources/Live.pmdl',
+		hotword: 'live'
+	},
+	{
+		file: './resources/Need.pmdl',
+		hotword: 'need'
+	},
+	{
+		file: './resources/Please.pmdl',
+		hotword: 'please'
+	},
+]
 
-var job;
+const sonus = Sonus.init({
+	hotwords,
+	language,
+	recordProgram,
+	device
+}, {})
 
-sendAudio();
+Sonus.start(sonus)
 
-async function sendAudio() {
-	console.log("Sending audio...")
-	console.log("")
+sonus.on('begging', (index, keyword) => console.log("begging"))
+sonus.on('die', (index, keyword) => console.log("die"))
+sonus.on('give', (index, keyword) => console.log("give"))
+sonus.on('have', (index, keyword) => console.log("have"))
+sonus.on('help', (index, keyword) => console.log("help"))
+sonus.on('let', (index, keyword) => console.log("let"))
+sonus.on('life', (index, keyword) => console.log("life"))
+sonus.on('live', (index, keyword) => console.log("live"))
+sonus.on('need', (index, keyword) => console.log("need"))
+sonus.on('please', (index, keyword) => console.log("please"))
 
-	job = await client.submitJobLocalFile("./tmp/pills_00.mp4");
+// var job;
 
-	setTimeout(pollEndpoint, 1500, job.id);
-}
+// sendAudio();
 
-// or from audio data, the filename is optional
-// const stream = fs.createReadStream("./path/to/file.mp3");
-// var job = await client.submitJobAudioData(stream, "file.mp3");
+// async function sendAudio() {
+// 	console.log("Sending audio...")
+// 	console.log("")
 
-// 2. Check if job complete
-// 	  Yes: move on to 3
-//    No: Keep polling
-// var jobDetails = await client.getJobDetails(job.id);
+// 	job = await client.submitJobLocalFile("./tmp/pills_00.mp4");
 
-async function pollEndpoint(jobId) {
-	console.log("Polling...")
-	console.log("")
+// 	setTimeout(pollEndpoint, 1500, job.id);
+// }
 
-	let jobDetails = await client.getJobDetails(jobId);
+// // or from audio data, the filename is optional
+// // const stream = fs.createReadStream("./path/to/file.mp3");
+// // var job = await client.submitJobAudioData(stream, "file.mp3");
 
-	if (jobDetails.status == "transcribed") {
-		getText(jobId);
-	} else {
-		setTimeout(pollEndpoint, 1500, jobId);
-	}
-}
+// // 2. Check if job complete
+// // 	  Yes: move on to 3
+// //    No: Keep polling
+// // var jobDetails = await client.getJobDetails(job.id);
 
-// 3. Get transcript
-// as plain text
-// var transcriptText = await client.getTranscriptText(job.id);
+// async function pollEndpoint(jobId) {
+// 	console.log("Polling...")
+// 	console.log("")
 
-// or as an object
-// var transcriptObject = await client.getTranscriptObject(job.id);
+// 	let jobDetails = await client.getJobDetails(jobId);
 
-async function getText(jobId) {
-	console.log("Here's DATA!!!")
-	console.log("")
+// 	if (jobDetails.status == "transcribed") {
+// 		getText(jobId);
+// 	} else {
+// 		setTimeout(pollEndpoint, 1500, jobId);
+// 	}
+// }
 
-	let transcriptObject = await client.getTranscriptObject(job.id);
+// async function getText(jobId) {
+// 	console.log("Here's DATA!!!")
+// 	console.log("")
 
-	console.log(transcriptObject.monologues[0].elements);
-}
+// 	let transcriptObject = await client.getTranscriptObject(job.id);
+
+// 	console.log(transcriptObject.monologues[0].elements);
+// }
