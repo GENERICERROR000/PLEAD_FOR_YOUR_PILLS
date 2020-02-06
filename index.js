f80ba6cbca22e5df08dbde90a56a9c356751623c
Noah Kernis <nkernis@gmail.com>
2020-02-06 17:04:56 -0500

    snowboy

--- index.js
+++ index.js
@@ -1,114 +1,90 @@
+const Snowboy = require('snowboy');
 // const Gpio = require('pigpio').Gpio;
-const Sonus = require('sonus')
+const record = require('node-record-lpcm16');
 
-// const accessToken = process.env.REVAI_TOKEN;
+const Detector = Snowboy.Detector;
+const Models = Snowboy.Models;
 
-const language = "en-US"
-const recordProgram = "arecord"
-const device = "plughw:1,0"
+const models = new Models();
+
 const hotwords = [
-	{
-		file: './resources/Begging.pmdl',
-		hotword: 'begging'
-	},
-	{
-		file: './resources/Die.pmdl',
-		hotword: 'die'
-	},
-	{
-		file: './resources/Give.pmdl',
-		hotword: 'give'
-	},
-	{
-		file: './resources/Have.pmdl',
-		hotword: 'have'
-	},
-	{
-		file: './resources/Help.pmdl',
-		hotword: 'help'
-	},
-	{
-		file: './resources/Let.pmdl',
-		hotword: 'let'
-	},
-	{
-		file: './resources/Life.pmdl',
-		hotword: 'life'
-	},
-	{
-		file: './resources/Live.pmdl',
-		hotword: 'live'
-	},
-	{
-		file: './resources/Need.pmdl',
-		hotword: 'need'
-	},
-	{
-		file: './resources/Please.pmdl',
-		hotword: 'please'
-	},
+	'begging',
+	'die',
+	'give',
+	'have',
+	'help',
+	'let',
+	'life',
+	'live',
+	'need',
+	'please'
 ]
 
-const sonus = Sonus.init({
-	hotwords,
-	language,
-	recordProgram,
-	device
-}, {})
+hotwords.forEach(hotword => {
+	models.add({
+		file: "./resources/" + hotword + ".umdl",
+		sensitivity: '0.5',
+		hotwords: hotword
+	})
+})
 
-Sonus.start(sonus)
+const detector = new Detector({
+	resource: "resources/common.res",
+	models: models,
+	audioGain: 2.0,
+	applyFrontend: true
+});
 
-sonus.on('begging', (index, keyword) => console.log("begging"))
-sonus.on('die', (index, keyword) => console.log("die"))
-sonus.on('give', (index, keyword) => console.log("give"))
-sonus.on('have', (index, keyword) => console.log("have"))
-sonus.on('help', (index, keyword) => console.log("help"))
-sonus.on('let', (index, keyword) => console.log("let"))
-sonus.on('life', (index, keyword) => console.log("life"))
-sonus.on('live', (index, keyword) => console.log("live"))
-sonus.on('need', (index, keyword) => console.log("need"))
-sonus.on('please', (index, keyword) => console.log("please"))
+detector.on('silence', function () {
+	console.log('silence');
+});
 
-// var job;
+detector.on('sound', function (buffer) {
+	// <buffer> contains the last chunk of the audio that triggers the "sound"
+	// event. It could be written to a wav stream.
+	console.log('sound');
+});
 
-// sendAudio();
+detector.on('error', function () {
+	console.log('error');
+});
 
-// async function sendAudio() {
-// 	console.log("Sending audio...")
-// 	console.log("")
+detector.on('hotword', function (index, hotword, buffer) {
+	// <buffer> contains the last chunk of the audio that triggers the "hotword"
+	// event. It could be written to a wav stream. You will have to use it
+	// together with the <buffer> in the "sound" event if you want to get audio
+	// data after the hotword.
+	// console.log(buffer);
+	console.log('hotword', index, hotword);
+});
 
-// 	job = await client.submitJobLocalFile("./tmp/pills_00.mp4");
+const mic = record.start({
+	threshold: 0,
+	verbose: true
+});
 
-// 	setTimeout(pollEndpoint, 1500, job.id);
-// }
+mic.pipe(detector);
 
-// // or from audio data, the filename is optional
-// // const stream = fs.createReadStream("./path/to/file.mp3");
-// // var job = await client.submitJobAudioData(stream, "file.mp3");
+// const language = "en-US"
+// const recordProgram = "arecord"
+// const device = "plughw:1,0"
 
-// // 2. Check if job complete
-// // 	  Yes: move on to 3
-// //    No: Keep polling
-// // var jobDetails = await client.getJobDetails(job.id);
+// const sonus = Sonus.init({
+// 	hotwords,
+// 	language,
+// 	recordProgram,
+// 	device
+// }, {})
 
-// async function pollEndpoint(jobId) {
-// 	console.log("Polling...")
-// 	console.log("")
+// Sonus.start(sonus)
 
-// 	let jobDetails = await client.getJobDetails(jobId);
-
-// 	if (jobDetails.status == "transcribed") {
-// 		getText(jobId);
-// 	} else {
-// 		setTimeout(pollEndpoint, 1500, jobId);
-// 	}
-// }
-
-// async function getText(jobId) {
-// 	console.log("Here's DATA!!!")
-// 	console.log("")
-
-// 	let transcriptObject = await client.getTranscriptObject(job.id);
-
-// 	console.log(transcriptObject.monologues[0].elements);
-// }
+// sonus.on('begging', (index, keyword) => console.log("begging"))
+// sonus.on('die', (index, keyword) => console.log("die"))
+// sonus.on('give', (index, keyword) => console.log("give"))
+// sonus.on('have', (index, keyword) => console.log("have"))
+// sonus.on('help', (index, keyword) => console.log("help"))
+// sonus.on('let', (index, keyword) => console.log("let"))
+// sonus.on('life', (index, keyword) => console.log("life"))
+// sonus.on('live', (index, keyword) => console.log("live"))
+// sonus.on('need', (index, keyword) => console.log("need"))
+// sonus.on('please', (index, keyword) => console.log("please"))
